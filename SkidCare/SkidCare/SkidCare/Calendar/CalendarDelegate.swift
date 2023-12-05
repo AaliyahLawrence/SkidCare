6a72425c848be8ed34ea54608afaee15abd32418
AaliyahLawrence <86375626+AaliyahLawrence@users.noreply.github.com>
2023-12-05 10:54:43 -0500

    Add files via upload

--- SkidCare/SkidCare/SkidCare/Calendar/CalendarDelegate.swift
+++ SkidCare/SkidCare/SkidCare/Calendar/CalendarDelegate.swift
@@ -13,3 +13,45 @@ class CalendarDelegate: NSObject, UICalendarViewDelegate{
         return .none
     }
 }
+/*
+ @MainActor
+ 
+ var calendarView: UICalendarView = {
+ let calendarObject = UICalendarView()
+ calendarObject.calendar = Calendar(identifier: .gregorian)
+ 
+ return calendarObject
+ }()
+ 
+ var calendarDelegate: CalendarDelegate!
+ 
+ override func viewDidLoad() {
+ super.viewDidLoad()
+ 
+ calendarDelegate = CalendarDelegate()
+ calendarView.delegate = calendarDelegate
+ 
+ view.backgroundColor = .white
+ title = "Calendar"
+ }
+ 
+ 
+ func setupUI() {
+ 
+ view.addSubview(calendarView)
+ 
+ calendarView.translatesAutoresizingMaskIntoConstraints = false
+ calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+ calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+ calendarView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+ calendarView.heightAnchor.constraint(equalToConstant: 500).isActive = true
+ 
+ 
+ 
+ }
+ 
+ 
+ 
+ 
+ }
+ */
--- SkidCare/SkidCare/SkidCare/ViewController.swift
+++ SkidCare/SkidCare/SkidCare/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import EventKit
+import EventKitUI
 
 class ViewController: UIViewController {
     
@@ -13,8 +15,8 @@ class ViewController: UIViewController {
     
     //Ideally would be a log-in page for skidmore
     private let button: UIButton = {
-        let button = UIButton()
-        button.setTitle("Log IN", for: .normal)
+        let button = UIButton(frame: CGRect(x:1,y:1,width: 200, height: 45))
+        button.setTitle("Log In", for: .normal)
         button.backgroundColor = .white
         button.setTitleColor(.green, for: .normal)
         return button
@@ -38,31 +40,33 @@ class ViewController: UIViewController {
     
     @objc func didTapButton() {
         let tabBarVC = UITabBarController()
-        let vc1 = UINavigationController(rootViewController: HomeViewController())
-        vc1.title = "Home"
         let vc2 = UINavigationController(rootViewController: MessageViewController())
-        vc2.title = "Messages"
+        vc2.title = "Alerts"
         let vc3 = UINavigationController(rootViewController: CalendarViewController())
         vc3.title = "Calendar"
         let vc4 = UINavigationController(rootViewController: PrescriptionViewController())
-        vc4.title = "Lab Results"
+        vc4.title = "Prescriprions"
         let vc5 = UINavigationController(rootViewController: LabViewController())
-        vc5.title = "Prescriptions"
+        vc5.title = "Lab Results"
         
-        tabBarVC.setViewControllers([vc1,vc2,vc3,vc4,vc5], animated: false)
+        tabBarVC.setViewControllers([vc5,vc3,vc4,vc2], animated: false)
         
         guard let items = tabBarVC.tabBar.items else{
             return
         }
         
-        let images = ["house","ellipsis.message","calendar", "list.clipboard", "pills"]
+        let images = ["house","alert","calendar", "list.clipboard", "pills"]
         
         for x in 0..<items.count{
             if(x % 2==0){
                 items[x].badgeValue = "2"
-                items[x].image = UIImage(systemName: images[x])
+                
             }
         }
+        items[1].image = UIImage(systemName: images[2])
+        items[0].image = UIImage(systemName: images[3])
+        items[2].image = UIImage(systemName: images[4])
+        items[3].image = UIImage(systemName: images[1])
         
         tabBarVC.modalPresentationStyle = .fullScreen
         present(tabBarVC, animated: true)
@@ -70,22 +74,6 @@ class ViewController: UIViewController {
 
 }
 
-/*
- * THIS IS CODE FOR THE HOMEPAGE
- *
- * TO INCLUDE:
- *  - Profile photo page
- *  - Navigation to other pages
- *  - Include the notification Text example Fields
- */
-
-class HomeViewController: UIViewController{
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemPurple
-        title = "Home"
-    }
-}
 
 /*
  * MESSAGE PAGE
@@ -100,7 +88,7 @@ class MessageViewController: UIViewController, UITableViewDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemOrange
-        title = "Messages"
+        title = "Alerts"
     }
 }
 
@@ -111,6 +99,7 @@ class MessageViewController: UIViewController, UITableViewDelegate{
  * SOURCES:
  * https://developer.apple.com/documentation/uikit/uicalendarview
  * https://medium.com/@ios_guru/custom-view-for-showing-a-calendar-1b512b0f772d
+ * https://www.youtube.com/watch?v=B_VFHeg2LH4
  *
  * TO INCLUDE:
  *  - A calendar view lmao
@@ -120,47 +109,69 @@ class MessageViewController: UIViewController, UITableViewDelegate{
  *  - Navigation to other pages
  */
 
-class CalendarViewController: UIViewController{
-    @MainActor
-   
-    var calendarView: UICalendarView = {
-        let calendarObject = UICalendarView()
-        calendarObject.calendar = Calendar(identifier: .gregorian)
-        
-        return calendarObject
-    }()
-    
-    var calendarDelegate: CalendarDelegate!
+class CalendarViewController: UIViewController,UICalendarViewDelegate, EKEventViewDelegate{
+    let apptStore = EKEventStore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        calendarDelegate = CalendarDelegate()
-        calendarView.delegate = calendarDelegate
-        
         view.backgroundColor = .white
-        title = "Calendar"
+        createCalendar()
+        addEvents()
     }
     
     
-    func setupUI() {
+ 
+    func createCalendar(){
+        let calendarView = UICalendarView()
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        
+        calendarView.calendar = .current
+        calendarView.locale = .current
+        calendarView.fontDesign = .rounded
+        calendarView.delegate = self
+        calendarView.layer.cornerRadius = 12
+        calendarView.backgroundColor = .white
+        
         
         view.addSubview(calendarView)
         
-        calendarView.translatesAutoresizingMaskIntoConstraints = false
-        calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-        calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-        calendarView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
-        calendarView.heightAnchor.constraint(equalToConstant: 500).isActive = true
-        
-    
-        
+        NSLayoutConstraint.activate([
+            calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            calendarView.heightAnchor.constraint(equalToConstant: 300),
+            calendarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        ])
+    }
+    func addEvents(){
+       
+        func addButton(_ sender: Any){
+            apptStore.requestWriteOnlyAccessToEvents()
+            { [weak self] success, error in
+                if success, error == nil{
+                    DispatchQueue.main.async {
+                        guard let store = self?.apptStore else {return}
+                        
+                        let newAppt = EKEvent(eventStore: store)
+                        newAppt.title = "Schedule Appointment"
+                        newAppt.startDate = Date()
+                        
+                        let eventController = EKEventViewController()
+                        eventController.delegate = self
+                        eventController.event = newAppt
+                        self?.present(eventController, animated: true, completion: nil)
+                    }
+                }
+                
+            }
+        }
     }
    
-        
-    
-    
+    func eventViewController(_ controller: EKEventViewController, didCompleteWith action: EKEventViewAction) {
+        //pls idc
+    }
+
 }
+    
 
 /*
  * PRESCRIPTIONS
@@ -303,16 +314,100 @@ class PrescriptionViewController: UIViewController, UITableViewDelegate,UITableV
      *  TO INCLUDE:
      *  - Cloudkit/core data on whatever lab results given (can't really read skidmore data so idk)
      *  - Navigation to other pages
+     * https://developer.apple.com/documentation/uikit/uiimage/creating_custom_symbol_images_for_your_app
      */
     
-    class LabViewController: UIViewController{
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            view.backgroundColor = .systemMint
-            title = "Lab Results"
-        }
+class LabViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+
+    
+           enum Status {
+               case check, cross, caution
+
+               var image: UIImage? {
+                   switch self {
+                   case .check: return UIImage(systemName: "checkmark.circle.fill")
+                   case .cross: return UIImage(systemName: "xmark.circle.fill")
+                   case .caution: return UIImage(systemName: "exclamationmark.circle.fill")
+                   }
+               }
+           }
+
+           let statusImageView: UIImageView = {
+               let imageView = UIImageView()
+               imageView.translatesAutoresizingMaskIntoConstraints = false
+               imageView.tintColor = UIColor.systemGreen // Change the color as needed
+               return imageView
+           }()
+
+           let tableView: UITableView = {
+               let tableView = UITableView()
+               tableView.translatesAutoresizingMaskIntoConstraints = false
+               return tableView
+           }()
+    
+            //Hardcoded lab results for the time being
+           let labResults: [String] = ["COVID-Test", "Iron Levels", "A1C Levels"]
+
+    
+           var currentStatus: Status = .check {
+               didSet {
+                   statusImageView.image = currentStatus.image
+               }
+           }
+
+           override func viewDidLoad() {
+               super.viewDidLoad()
+
+               setUp()
+
+               tableView.delegate = self
+               tableView.dataSource = self
+               tableView.register(UITableViewCell.self, forCellReuseIdentifier: "LabResultCell")
+           }
+
+           private func setUp() {
+               view.backgroundColor = UIColor.white
+
+               view.addSubview(statusImageView)
+               view.addSubview(tableView)
+
+               NSLayoutConstraint.activate([
+                   statusImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+                   statusImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                   statusImageView.heightAnchor.constraint(equalToConstant: 50),
+                   statusImageView.widthAnchor.constraint(equalToConstant: 50),
+
+                   tableView.topAnchor.constraint(equalTo: statusImageView.bottomAnchor, constant: 20),
+                   tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                   tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                   tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+               ])
+           }
+
+
+           func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+               return labResults.count
+           }
+
+           func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+               let cell = tableView.dequeueReusableCell(withIdentifier: "LabResultCell", for: indexPath)
+               cell.textLabel?.text = labResults[indexPath.row]
+               return cell
+           }
+    }
         
         
+        
+    
+extension ViewController: UICalendarViewDelegate,UICalendarSelectionSingleDateDelegate{
+    func calendarView(_ calendarView: UICalendarView, decorationFor dateComponents: DateComponents) -> UICalendarView.Decoration? {
+        return nil
     }
     
-
+    func dateSelection(_ selection: UICalendarSelectionSingleDate, didSelectDate dateComponents: DateComponents?) {
+        //
+    }
+    
+   
+    
+}
